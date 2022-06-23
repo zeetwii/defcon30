c5572ed886db18f5f0ae5abff19f951bc4c185f9
Nick Ashworth <jna2188@utulsa.edu>
2022-06-23 16:24:25 -0400

    edits

--- lbnl/editor.py
+++ lbnl/editor.py
@@ -1,12 +1,27 @@
 import netCDF4 # needed to read and edit netCDF4 files
 import shutil # needed to make a copy of the file
 import glob # needed to search current directory
+import sys # needed to exit
 
 
 files = glob.glob('./*.nc')
 
-for file in files:
-    print(file)
+if len(files) < 1:
+    print("No netCDF4 files found in current directory")
+    print("Please make sure that all netCDF files are using '.nc' as a file extension")
+    sys.exit(1)
+else:
+    print("List of '.nc' files:")
+    for i in range(len(files)):
+        print(f'File {str(i)} : {str(files[i])}')
+
+fileChoice = input('Enter the number of the file to use, or anything to exit:')
+
+if str(fileChoice).isdigit():
+    print(f'Using {str(files[int(fileChoice)])}')
+else:
+    print('Not an int, exiting')
+    sys.exit()
 
 # start with a clean file
 #shutil.copyfile('thredds_conf/content/thredds/public/best.bak', 'thredds_conf/content/thredds/public/best')
